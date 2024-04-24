0f0ae3ea781655664238934048fa8b320af17298
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-04-24 17:17:59 +0530

    Implemented show password / hide password in both pages

--- frontend/src/pages/SignInPage.jsx
+++ frontend/src/pages/SignInPage.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SignIn from "../assets/Sign_in.png"
+import { useNavigate } from 'react-router-dom'
+import { Eye, EyeOff } from 'lucide-react'
 
 const SignInPage = () => {
+    const navigate = useNavigate()
+    const [isShow, setIsShow] = useState(false)
+
+    const toggleState = () => {
+        setIsShow(!isShow)
+    }
+
+
+    const goToNewPage = () => {
+        navigate("/signup")
+    }
     return (
         <div className='md:flex'>
             <div className='flex justify-center md:justify-end w-full md:w-2/4'>
@@ -10,14 +23,21 @@ const SignInPage = () => {
             <div className='flex flex-col justify-center items-center md:items-start w-full md:w-2/4'>
                 <div className='relative border m-5 md:m-14 p-5 md:p-10 w-full md:w-[400px] h-auto md:h-[400px] md:-top-8 rounded-lg'>
                     <div>
-                        <h1 className='font-extrabold text-3xl'>Fill what we knows <span className='text-[#D72638]'>!</span></h1>
+                        <h1 className='font-extrabold text-3xl text-[#3A244A]'>Fill what we knows <span className='text-[#D72638]'>!</span></h1>
                     </div>
                     <form action="" className='flex flex-col gap-4 pt-6'>
-                        <input type="email" placeholder='email' className='border-b h-10 p-2' />
-                        <input type="password" placeholder='password' className='border-b h-10 p-2' />
+                        <div className='border-b'>
+                            <input type="email" placeholder='email' className='w-[320px] h-10 p-2' />
+                        </div>
+                        <div className='border-b flex justify-between'>
+                            <input type={isShow ? "text" : "password"} placeholder='password' className='w-[410px] h-10 p-2' />
+                            <div onClick={toggleState} className='self-center'>
+                                {isShow ? <Eye color='#3A244A' /> : <EyeOff color='#3A244A' />}
+                            </div>
+                        </div>
                         <div className='flex flex-col gap-4'>
                             <button type='submit' className='bg-[#3A244A] p-3 rounded-2xl text-white font-semibold w-full md:w-auto'>Sign In</button>
-                            <button className='bg-[#FFFFFF] border-[#3A244A] border-2 p-3 rounded-2xl text-[#3A244A] font-semibold w-full md:w-auto'>Sign Up</button>
+                            <button onClick={() => goToNewPage()} className='bg-[#FFFFFF] border-[#3A244A] border-2 p-3 rounded-2xl text-[#3A244A] font-semibold w-full md:w-auto'> Sign Up</button>
                         </div>
                     </form>
                 </div>
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SignUp from "../assets/signup.png"
+import { Link } from 'react-router-dom'
+import { Eye, EyeOff } from 'lucide-react'
 
 const SignUpPage = () => {
+    const [showPassword, setShowPassword] = useState(false)
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword)
+    }
+
+    const toggleConfirmPasswordVisibility = () => {
+        setShowConfirmPassword(!showConfirmPassword)
+    }
+
     return (
-        <div>SignUpPage</div>
+        <div className='md:flex'>
+            <div className='flex justify-center md:justify-end w-full md:w-2/4'>
+                <img src={SignUp} alt="" className='h-auto md:h-[1000px] w-full md:w-auto md:relative md:left-28 ' />
+            </div>
+            <div className='flex flex-col justify-center items-center md:items-start w-full md:w-2/4'>
+                <div className='relative border m-5 md:m-14 p-5 md:p-10 w-full md:w-[450px] h-auto  md:-top-8 rounded-lg'>
+                    <div className='flex justify-between'>
+                        <h1 className='font-extrabold text-3xl text-[#3A244A]'>Let us know <span className='text-[#D72638]'>!</span></h1>
+                        <Link to={'/signin'}>
+                            <a href="" className='self-end text-[#3A244A] font-semibold underline'> Sign <span className='text-[#D72638] underline'>In</span> </a>
+                        </Link>
+                    </div>
+                    <form action="" className='flex flex-col gap-4 pt-6'>
+                        <div className=' border-b'>
+                            <input type="text" placeholder='First Name' className='w-[370px] h-10 p-2' />
+                        </div>
+                        <div className='border-b'>
+                            <input type="text" placeholder='Last Name' className='w-[370px] h-10 p-2' />
+                        </div>
+                        <div className='border-b flex justify-between'>
+                            <input type={showPassword ? "text" : "password"} placeholder='Set Password' className='w-[370px] h-10 p-2' />
+                            <div onClick={togglePasswordVisibility} className='self-center'>
+                                {showPassword ? <Eye color='#3A244A' /> : <EyeOff color='#3A244A' />}
+                            </div>
+                        </div>
+                        <div className='border-b flex justify-between'>
+                            <input type={showConfirmPassword ? "text" : "password"} placeholder='Retype Password' className='w-[370px] h-10 p-2' />
+                            <div onClick={toggleConfirmPasswordVisibility} className='self-center'>
+                                {showConfirmPassword ? <Eye color='#3A244A' /> : <EyeOff color='#3A244A' />}
+                            </div>
+                        </div>
+                        <div className='border-b flex justify-between h-10'>
+                            <label htmlFor="contact" className='self-center pl-2 text-gray-400'>Contact Mode</label>
+                            <select name="" placeholder="Contact mode" id="">
+                                <option value="email">Email</option>
+                                <option value="phone">Phone</option>
+                            </select>
+                        </div>
+                        <div className='border-b'>
+                            <input type="email" placeholder='Enter Email' className='w-[370px] h-10 p-2' />
+                        </div>
+                        <div className='flex flex-col gap-4'>
+                            <button type='submit' className='bg-[#3A244A] p-3 rounded-2xl text-white font-semibold w-full md:w-auto'>Sign Up</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
