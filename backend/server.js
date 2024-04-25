0146d76f3573ceee1569bd3df5f85f64d769aea2
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-04-25 15:24:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ async function main() {
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: [""], credentials: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
