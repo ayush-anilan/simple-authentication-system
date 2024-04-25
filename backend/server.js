4560f7ea739b5760ff5a362d3fa62e61897ecd97
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-04-25 18:55:30 +0530

    testing deployment

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,10 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
+
 // Route
 const userRouter = require("./routes/users");
 app.use("/", userRouter);
