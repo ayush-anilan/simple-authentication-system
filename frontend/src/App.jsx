b38c3a792c88b567542cd15ed09f8734ec7c2c8c
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-04-25 22:25:31 +0530

    updated app.jsx and create proxy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
-import axios from 'axios'
 import SignInPage from './pages/SignInPage'
 import SignUpPage from './pages/SignUpPage'
 import IndexPage from './pages/IndexPage'
 import ResetPasswordPage from './pages/ResetPasswordPage'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
-axios.defaults.withCredentials = true
+
 
 
 function App() {
