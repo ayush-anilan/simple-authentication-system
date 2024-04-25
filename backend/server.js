cb3dd9f1c81db0d6a2ba05885dea69e24afd2e35
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-04-25 22:20:04 +0530

    updated cors in server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:5000",
+      "https://highway-delite-task.onrender.com",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
