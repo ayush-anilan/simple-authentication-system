718f8261c6f13beefc4ebc7f04609e503de1b703
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-04-25 20:26:22 +0530

    pre deployment fixes 2

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,6 @@ async function main() {
   console.log("Connected to database");
 }
 
-const __dirname = path.resolve();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -27,6 +25,7 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
