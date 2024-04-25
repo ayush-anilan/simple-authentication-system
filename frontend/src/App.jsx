6a5086ee8603237196392e8988b28cbce4a1d0b4
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-04-25 15:21:47 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import SignUpPage from './pages/SignUpPage'
 import IndexPage from './pages/IndexPage'
 import ResetPasswordPage from './pages/ResetPasswordPage'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
+axios.defaults.baseURL = https://highway-delite-task-api.vercel.app
 axios.defaults.withCredentials = true
 
 
