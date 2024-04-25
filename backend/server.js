77174559c854b177a785eea3f56ccadd86920810
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-04-25 22:15:37 +0530

    update cors in server.ks

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: true,
+    origin: ["http://localhost:3000"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 app.use(express.json());
